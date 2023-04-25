33bf3265895863e435d8b66d15744b080abb9a2d
Igor Alvarez <igort.lira@gmail.com>
2023-04-25 00:05:02 -0300

    Configurando o document e preparando a Home Page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="pt-br">
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta charSet="utf-8" />
+        <meta name="description" content="Portfolio do Diogo Rother" />
+        <meta name="author" content="Igor Alvarez" />
+        <meta name="keywords" content="Diogo Rother, Diogo Rother Portfolio" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex, VStack } from '@chakra-ui/react';
 
-const index = () => {
-  return <Flex>Olá</Flex>;
+const HomePage = () => {
+  return (
+    <Flex w="full" h="full">
+      <VStack>
+        <Box bg="tomato" w="100%" p={4} color="white">
+          This is the Box
+        </Box>
+        <Box bg="tomato" w="100%" p={4} color="white">
+          This is the Box
+        </Box>
+        <Box bg="tomato" w="100%" p={4} color="white">
+          This is the Box
+        </Box>
+      </VStack>
+    </Flex>
+  );
 };
 
-export default index;
+export default HomePage;
